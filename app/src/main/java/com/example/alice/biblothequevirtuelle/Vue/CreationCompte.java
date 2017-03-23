b8073f71daa6524a74a729037e8e8103e6096fa6
aud04 <aud.stephan@gmail.com>
2017-03-23 12:29:36 +0100

    Création de la classe CreationCompte à continuer

--- app/src/main/java/com/example/alice/biblothequevirtuelle/Vue/CreationCompte.java
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Vue/CreationCompte.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.alice.biblothequevirtuelle.Data.Utilisateur;
+import com.example.alice.biblothequevirtuelle.R;
+
 import java.io.IOException;
 
 /**
@@ -22,22 +25,23 @@ public class CreationCompte extends Activity {
 
     public void ClickValider(View v) throws IOException {
 
-        // Ajout dans la BDD SQLite
-       /** try{
-            EditText number = (EditText) findViewById(R.id.number);
-            EditText nom = (EditText) findViewById(R.id.nom);
-            EditText prenom = (EditText) findViewById(R.id.prenom);
 
+       try{
+            EditText pseudo = (EditText) findViewById(R.id.etPseudoInscription);
+            EditText nom = (EditText) findViewById(R.id.etNomInscription);
+            EditText prenom = (EditText) findViewById(R.id.etPrenomInscription);
+            EditText motDePasse = (EditText) findViewById(R.id.etMdpInscription1);
+            EditText email = (EditText) findViewById(R.id.etMailInscription);
 
-            Utilisateur c=new Personne(nom.getText().toString(),prenom.getText().toString(),number.getText().toString());
-            c.save();
-            Toast toast = Toast.makeText(getApplicationContext(), nom.getText().toString() + " " + prenom.getText().toString() + " a été ajouté", Toast.LENGTH_LONG);
+            Utilisateur u = new Utilisateur (pseudo.getText().toString(),nom.getText().toString(),prenom.getText().toString(), motDePasse.getText().toString(), email.getText().toString());
+            u.save();
+            Toast toast = Toast.makeText(getApplicationContext(), nom.getText().toString() + " " + prenom.getText().toString() + " a bien été crée et ajouté", Toast.LENGTH_LONG);
             toast.show();
         }
 
         catch (Exception e){
-            Toast.makeText(this, "Le contact n'a pas pu être inséré", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "L'utilisateur n'a pas pu être crée", Toast.LENGTH_SHORT).show();
         }
-    }**/
+    }
 }
-}
+
