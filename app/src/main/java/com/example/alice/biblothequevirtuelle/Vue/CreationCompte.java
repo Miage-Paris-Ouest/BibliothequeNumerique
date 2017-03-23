040272fb7d2f02d617f4ff26bfd4768135f651f3
aud04 <aud.stephan@gmail.com>
2017-03-23 11:58:13 +0100

    Création de la classe CreationCompte

---
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Vue/CreationCompte.java
@@ -0,0 +1,13 @@
+package com.example.alice.biblothequevirtuelle.Vue;
+
+/**
+ * Created by austepha on 23/03/2017.
+ */
+
+public class CreationCompte {
+
+    //classe qui permet de mettre un nouvel utilisateur dans la base de données
+
+
+
+}
