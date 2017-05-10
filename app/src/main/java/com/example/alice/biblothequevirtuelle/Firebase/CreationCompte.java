c71e0c152f3c9de52ff09adfa11989a8a0e305de
aud04 <aud.stephan@gmail.com>
2017-05-10 09:39:43 +0200

    Merge branch 'dev' of C:\AndroidWork\BibliothequeNumerique with conflicts.

--- app/src/main/java/com/example/alice/biblothequevirtuelle/Firebase/CreationCompte.java
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Firebase/CreationCompte.java
@@ -1,70 +1,110 @@
 package com.example.alice.biblothequevirtuelle.Firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.alice.biblothequevirtuelle.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * Created by Audrey on 04/04/2017.
- */
+public class CreationCompte extends AppCompatActivity {
 
-public class CreationCompte extends AppCompatActivity implements View.OnClickListener {
 
     private Button bValiderInscription;
-    private EditText etPseudoInscription;
-    private EditText etNomInscription;
-    private EditText etPrenomInscription;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
     private EditText etMdpInscription1;
-    private EditText etMdpInscription2;
     private EditText etMailInscription;
-    private TextView tvSignin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_inscription_layout);
 
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
         bValiderInscription = (Button) findViewById(R.id.bValiderInscription);
-
         etMailInscription = (EditText) findViewById(R.id.etMailInscription);
-        etPseudoInscription = (EditText) findViewById(R.id.etPseudoInscription);
-        etNomInscription = (EditText) findViewById(R.id.etNomInscription);
-        etPrenomInscription = (EditText) findViewById(R.id.etPrenomInscription);
         etMdpInscription1 = (EditText) findViewById(R.id.etMdpInscription1);
-        etMdpInscription2 = (EditText) findViewById(R.id.etMdpInscription2);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        bValiderInscription.setOnClickListener(this);
-        tvSignin.setOnClickListener(this);
 
-    }
+        /*btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
+            }
+        });*/
 
-    private void registerUser(){
-        String email = etMailInscription.getText().toString().trim();
-        String password = etMdpInscription1.getText().toString().trim();
+        /*btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });*/
 
-        if(TextUtils.isEmpty(email)){
+        bValiderInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        }
+                String email = etMailInscription.getText().toString().trim();
+                String password = etMdpInscription1.getText().toString().trim();
 
-        if(TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-        }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(CreationCompte.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(CreationCompte.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(CreationCompte.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                }/* else {
+                                    startActivity(new Intent(SignupActivity.this, CreationCompte.class));
+                                    finish();
+                                }*/
+                            }
+                        });
+
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
-
-        if(v == bValiderInscription){
-            registerUser();
-        }
-        if(v == tvSignin){
-            //redirige vers la page de connection
-        }
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
