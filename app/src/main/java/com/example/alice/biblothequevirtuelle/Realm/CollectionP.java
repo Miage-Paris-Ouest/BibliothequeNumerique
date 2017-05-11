7a1aa005d26b57a638e87c0e08a72620279e2b19
AliceP-R <alice.pottier@free.fr>
2017-05-11 10:49:32 +0200

    Merge branch 'dev' of C:\Users\alice\Desktop\Perso\Fac\Projet PSI\BibliothequeNumerique with conflicts.

---
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Realm/CollectionP.java
@@ -0,0 +1,64 @@
+package com.example.alice.biblothequevirtuelle.Realm;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by alice on 11/05/2017.
+ */
+
+class CollectionP extends RealmObject
+{
+    @PrimaryKey
+    private int id;
+    private String nom;
+    private RealmList<Livre> livres;
+
+
+    public CollectionP(String nom) {
+        this.nom = nom;
+        this.livres = new RealmList<>();
+    }
+
+    public CollectionP(String nom, RealmList<Livre> livres) {
+        this.nom = nom;
+        this.livres = livres;
+    }
+
+    public CollectionP(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+        this.livres = new RealmList<>();
+    }
+
+    public CollectionP(int id, String nom, RealmList<Livre> livres) {
+        this.id = id;
+        this.nom = nom;
+        this.livres = livres;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public RealmList<Livre> getLivres() {
+        return livres;
+    }
+
+    public void setLivres(RealmList<Livre> livres) {
+        this.livres = livres;
+    }
+}
---
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Realm/Utilisateur.java
@@ -0,0 +1,106 @@
+package com.example.alice.biblothequevirtuelle.Realm;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by alice on 11/05/2017.
+ */
+
+public class Utilisateur extends RealmObject
+{
+    @PrimaryKey
+    private String firebaseID;
+    private String mail;
+    private String pseudo;
+    private RealmList<Livre> whishlist;
+    private RealmList<CollectionP> listeCollections;
+    private boolean dejaConnecte;
+
+    public Utilisateur(String mail, String firebaseID) {
+        this.mail = mail;
+        this.firebaseID = firebaseID;
+        this.dejaConnecte = false;
+        this.whishlist = new RealmList<>();
+        this.listeCollections = new RealmList<>();
+    }
+
+    public Utilisateur(String mail, String firebaseID, String pseudo, RealmList<Livre> wishlist, RealmList<CollectionP> listeCollections, boolean dejaConnecte) {
+        this.mail = mail;
+        this.firebaseID = firebaseID;
+        this.pseudo = pseudo;
+        this.whishlist = wishlist;
+        this.listeCollections = listeCollections;
+        this.dejaConnecte = dejaConnecte;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getFirebaseID() {
+        return firebaseID;
+    }
+
+    public void setFirebaseID(String firebaseID) {
+        this.firebaseID = firebaseID;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public RealmList<Livre> getWhishlist() {
+        return whishlist;
+    }
+
+    public void setWhishlist(RealmList<Livre> whishlist) {
+        this.whishlist = whishlist;
+    }
+
+    public RealmList<CollectionP> getListeCollections() {
+        return listeCollections;
+    }
+
+    public void setListeCollections(RealmList<CollectionP> listeCollections) {
+        this.listeCollections = listeCollections;
+    }
+
+    public boolean isDejaConnecte() {
+        return dejaConnecte;
+    }
+
+    public void setDejaConnecte(boolean dejaConnecte) {
+        this.dejaConnecte = dejaConnecte;
+    }
+
+    public void ajouterLivreWhishList(Livre l)
+    {
+        this.whishlist.add(l);
+    }
+
+    public void supprimerLivreWhishList(Livre l)
+    {
+        this.whishlist.remove(l);
+    }
+
+    public void creerCollection(String nom)
+    {
+        this.listeCollections.add(new CollectionP(nom));
+    }
+
+    public void supprimerCollection(int id)
+    {
+        CollectionP col = this.listeCollections.where().equalTo("id", id).findFirst();
+        this.listeCollections.remove(col);
+    }
+}
