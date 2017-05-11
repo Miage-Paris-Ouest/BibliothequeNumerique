c2a7f29b820374e73320d6549300272064ef5d93
Dodymimi <Kanzaki-angel@hotmail.fr>
2017-05-11 12:53:01 +0200

    Layout modification Inscription / Reset / Connexion

--- app/src/main/java/com/example/alice/biblothequevirtuelle/Firebase/ResetPasswordActivity.java
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Firebase/ResetPasswordActivity.java
@@ -30,7 +30,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reset_password);
+        setContentView(R.layout.reset_password_layout);
 
         etAdresseMail = (EditText) findViewById(R.id.etAdresseMail);
         bReset = (Button) findViewById(R.id.bReset);
