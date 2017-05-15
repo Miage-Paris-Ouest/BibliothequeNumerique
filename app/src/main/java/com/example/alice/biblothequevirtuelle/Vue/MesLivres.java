f2af73369ffdc79859b4d7a1edb073718c09c990
CamLeuregans <camleuregans@gmail.com>
2017-05-15 13:20:26 +0200

    Ajout du filtre dans mes livres

--- app/src/main/java/com/example/alice/biblothequevirtuelle/Vue/MesLivres.java
+++ app/src/main/java/com/example/alice/biblothequevirtuelle/Vue/MesLivres.java
@@ -3,8 +3,11 @@ package com.example.alice.biblothequevirtuelle.Vue;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -16,7 +19,9 @@ import com.example.alice.biblothequevirtuelle.Realm.Livre;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import io.realm.Case;
 import io.realm.Realm;
+import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 public class MesLivres extends AppCompatActivity {
@@ -26,6 +31,9 @@ public class MesLivres extends AppCompatActivity {
     ListView listLivres;
     Realm realm;
     RealmResults<Livre> realmResults;
+
+
+
 
     private class listeLivreClickHandler implements AdapterView.OnItemClickListener {
 
@@ -49,6 +57,28 @@ public class MesLivres extends AppCompatActivity {
 
 
         realm = Realm.getDefaultInstance();
+
+        //alimente la liste actvauteur et actvediteur
+        AutoCompleteTextView actvauteur = (AutoCompleteTextView) findViewById(R.id.actvAuteur);
+        AutoCompleteTextView actvediteur = (AutoCompleteTextView) findViewById(R.id.actvEditeur);
+        RealmResults<Livre> realmLivre = realm.where(Livre.class).findAll();
+        String[] listAuteur=new String[realmLivre.size()];
+        String[] listEditeur=new String[realmLivre.size()];
+        for(int i=0;i<realmLivre.size();i++)//créer la liste d'auteur
+        {
+            listAuteur[i]=realmLivre.get(i).getAuteur();
+            listEditeur[i]=realmLivre.get(i).getEditeur();
+        }
+        ArrayAdapter<String> auteuradapter = new ArrayAdapter<String>
+                (this, android.R.layout.select_dialog_item, listAuteur);
+        actvauteur.setThreshold(1);//suggestion commenc a 1
+        actvauteur.setAdapter(auteuradapter);
+        ArrayAdapter<String> editeuradapter = new ArrayAdapter<String>
+                (this, android.R.layout.select_dialog_item, listEditeur);
+        actvediteur.setThreshold(1);//suggestion commenc a 1
+        actvediteur.setAdapter(editeuradapter);
+
+
 
         adapter = new SimpleAdapter(this, donnees, R.layout.livre_liste_layout,
                 new String[]{"isbn", "titre", "auteur", "id"},
@@ -111,4 +141,74 @@ public class MesLivres extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
-}
+    public void onClickhercherEditeur(View view) {
+        AutoCompleteTextView actvediteur = (AutoCompleteTextView) findViewById(R.id.actvEditeur);
+        String editeur = actvediteur.getText().toString();
+
+        if(editeur.equals(""))
+        {
+            Toast.makeText(getApplicationContext(), "Vous n'avez pas entré d'editeur.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            try {
+
+                RealmQuery<Livre> rr = realm.where(Livre.class);
+                if (!editeur.equals("")) {
+                    rr.contains("editeur", editeur, Case.INSENSITIVE);
+                }
+                donnees.clear();
+
+                realmResults= rr.findAll();
+                if (realmResults.isEmpty()) {
+                    Toast.makeText(this.getBaseContext(), "Oups aucune correspondance...", Toast.LENGTH_LONG).show();
+                } else {
+                    for (Livre lv : realmResults) {
+                        addItem(lv.getEan(), lv.getTitre(), lv.getAuteur(),String.valueOf(lv.getId()));
+                    }
+                }
+
+                adapter.notifyDataSetChanged();
+
+            } catch (Exception e) {
+                Log.e("Main : ", e.toString());
+            }
+        }
+    }
+
+    public void onClickTriChercherAuteur(View view) {
+        AutoCompleteTextView actvauteur = (AutoCompleteTextView) findViewById(R.id.actvAuteur);
+        String auteur = actvauteur.getText().toString();
+
+
+            if(auteur.equals(""))
+            {
+                Toast.makeText(getApplicationContext(), "Vous n'avez pas entré d'auteur.", Toast.LENGTH_LONG).show();
+            }
+            else {
+                try {
+
+                    RealmQuery<Livre> rr = realm.where(Livre.class);
+                    if (!auteur.equals("")) {
+                        rr.contains("auteur", auteur, Case.INSENSITIVE);
+                    }
+                    donnees.clear();
+
+                    realmResults= rr.findAll();
+                    if (realmResults.isEmpty()) {
+                        Toast.makeText(this.getBaseContext(), "Oups aucune correspondance...", Toast.LENGTH_LONG).show();
+                    } else {
+                        for (Livre lv : realmResults) {
+                            addItem(lv.getEan(), lv.getTitre(), lv.getAuteur(), String.valueOf(lv.getId()));
+                        }
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                } catch (Exception e) {
+                    Log.e("Main : ", e.toString());
+                }
+            }
+        }
+    }
+
+
